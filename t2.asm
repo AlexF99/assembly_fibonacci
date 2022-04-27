88051812f58aa8c197d4cc55ccd77073c0e0dbd3
AlexF99 <alexandreopf@gmail.com>
2022-04-27 18:10:40 -0300

    inicio da implementacao do mul e main

--- t2.asm
+++ t2.asm
@@ -1,5 +1,18 @@
 MAIN:
+    addi r4, r0, 0 # i = 0
+    addi r5, r0, 0 # fat = 0
+    addi r6, r0, 0 # posicao inicial de fibVetor
+    addi r7, r0, 10 # posicao inicial de fatVetor
 
+    add r2, r0, r6
+    addi r3, r0, 10
+    jal r15, FIB
+
+    addi r8, r0, 5
+FORMAIN:
+    slt r9, r4, r8
+    beq r9, r0, HALT
+    # codigo do FORMAIN
 
 MUL:
     addi r1, r0, 0 # mul = 0
@@ -14,3 +27,6 @@ RETURN:
 
 
 FIB:
+
+HALT:
+    halt
